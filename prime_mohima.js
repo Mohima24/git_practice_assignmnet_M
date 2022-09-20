cb700fb7128e384bc2c79cb08310ccb0a2b9b646
unknown <mohimabahadur@gmail.com>
2022-09-20 22:51:46 +0530

    fixed by mohima

--- prime_mohima.js
+++ prime_mohima.js
@@ -2,6 +2,15 @@ let str ="abcba";
 
 let rev_str = "";
 
-for(let x = str.length-1 ; x <= 0 ; x--){
-    rev_str = rev_str + str[x];
+let num = 13;
+let count =0;
+for (let x = 0 ; x <= 13 ; x++){
+    if(num%x==0){
+        count++;
+    }
+}
+if(count==2){
+    console.log("prime")
+}else{
+    console.log("not prime")
 }
